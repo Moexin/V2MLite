d02217d78a1760a57b67941b4d42638f7c73fbb7
Moexin <moexin@foxmail.com>
2020-02-27 21:45:14 +0800

    fix

--- install.sh
+++ install.sh
@@ -2,34 +2,27 @@
 echo -n "请输入您的V2Ray服务端口（默认：12345）";
 read V2Ray_Port;
 echo "您输入的是：${V2Ray_Port:-12345}"
-
 echo -n "请输入您的V2Ray传输协议（默认：ws）";
 read V2Ray_Network;
 echo "您输入的是：${V2Ray_Network:-ws}"
-
 echo -n "请输入您的数据库地址（默认：127.0.0.1）";
 read Mysql_Host;
 echo "您输入的是：${Mysql_Host:-127.0.0.1}"
-
 echo -n "请输入您的数据库端口（默认：3306）";
 read Mysql_Port;
 echo "您输入的是：${Mysql_Port:-3306}"
-
 echo -n "请输入您的数据库表名（默认：MyV2Ray）";
 read Mysql_Db;
 echo "您输入的是：${Mysql_Db:-MyV2Ray}"
-
 echo -n "请输入您的数据库用户名（默认：MyV2Ray）";
 read Mysql_User;
 echo "您输入的是：${Mysql_User:-MyV2Ray}"
-
 echo -n "请输入您的数据库密码（默认：123456）";
 read Mysql_Password;
 echo "您输入的是：${Mysql_Password:-123456}"
-
 apt-get update -y
-apt-get install curl git supervisor -y
-wget https://install.direct/go.sh && chmod +x go.sh && ./go.sh
+apt-get install curl git unzip supervisor -y
+wget -N "https://install.direct/go.sh" && chmod +x go.sh && ./go.sh
 mkdir /root/v2mlite
 cd /root/v2mlite
 git clone -b master https://github.com/Moexin/V2MLite.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
@@ -54,6 +47,6 @@ systemctl stop v2ray
 sleep 3
 systemctl disable v2ray
 supervisorctl reload
-echo '部署完成,请等待5秒查看以下输出有无报错。'
+echo '部署完成,请等待5秒查看进程是否Runing。'
 sleep 6
 supervisorctl status
